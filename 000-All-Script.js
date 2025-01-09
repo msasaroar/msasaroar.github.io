8aa11a498083841f312ce8a74ff5d56993584848
Azfar Rabib Sakif <azfarrabibsakif@gmail.com>
2025-01-09 23:03:44 +0600

    1

--- 000-All-Script.js
+++ 000-All-Script.js
@@ -2,7 +2,17 @@
 //    <script src="601-2-JavaScript.js"></script>
 //    <button onclick="navigateToURL()">Show Google Reference</button>
 
-function navigateToURL(recommendedEventName) {
+function openFile(fileName) {
+    if (fileName) {
+      // Logic to open the file, for example:
+      window.location.href = fileName;
+    } else {
+      alert("Please select a file first.");
+    }
+  }
+
+
+  function navigateToURL(recommendedEventName) {
     const baseURL = "https://developers.google.com/analytics/devguides/collection/ga4/reference/events?client_type=gtm#";
     const finalURL = baseURL + recommendedEventName;
     window.location.href = finalURL;
